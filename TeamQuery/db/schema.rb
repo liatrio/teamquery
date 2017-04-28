95c6565dde4ccc67dd136c9038f18b51a07cd6c1
jguzman4 <jguzman23@mail.csuchico.edu>
2017-04-28 12:30:52 -0700

    authentication almost working

--- TeamQuery/db/schema.rb
+++ TeamQuery/db/schema.rb
@@ -22,10 +22,10 @@ ActiveRecord::Schema.define(version: 20170422194151) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "password"
     t.integer  "company_id"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "users", ["company_id"], name: "index_users_on_company_id"
