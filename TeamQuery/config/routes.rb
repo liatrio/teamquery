098028dd3ab5507b1864ebbd6a75530272dbd838
jguzman4 <jguzman23@mail.csuchico.edu>
2017-05-01 15:33:11 -0700

    testing surveys branch

--- TeamQuery/config/routes.rb
+++ TeamQuery/config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
 
+  get '/survey' => 'surveys#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
