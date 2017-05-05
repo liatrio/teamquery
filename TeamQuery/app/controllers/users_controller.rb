0d0991951033f7e5dbc6b4e729f532b73e31c3b7
Nelsonater <alexn@liatrio.com>
2017-05-05 14:25:34 -0700

    Added company dropdown to user creation

--- TeamQuery/app/controllers/users_controller.rb
+++ TeamQuery/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
         def new
           @user = User.new
+          @companies = Company.all
         end
         def create
                 @user = User.new(user_params)
