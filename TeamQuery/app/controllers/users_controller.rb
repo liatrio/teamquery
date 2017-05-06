776c09993c99569f1440d55eab2c087a63ed38e9
HunterMayers <huntercmayers@gmail.com>
2017-05-06 11:25:21 -0700

    fixed company association with creation

--- TeamQuery/app/controllers/users_controller.rb
+++ TeamQuery/app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
         end
         private
           def user_params
-                params.require(:user).permit(:name, :email, :password)
+                params.require(:user).permit(:name, :email, :password, :company_id)
           end
 end
