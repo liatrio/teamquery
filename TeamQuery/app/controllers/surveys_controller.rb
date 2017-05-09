86329716d5fc2ca96d220a814d12d6413fb486ea
Chad Duffin <chadduffin01@gmail.com>
2017-05-09 10:47:49 -0700

    Successfully created surveys, questions, able to view them and everyone in a company gets surveys created by their peers.

--- TeamQuery/app/controllers/surveys_controller.rb
+++ TeamQuery/app/controllers/surveys_controller.rb
@@ -1,48 +1,41 @@
 class SurveysController < ApplicationController
+  before_action :require_user
   def index
-    @surveys = Survey.all
-    @questions = Question.all
+    @surveys = Survey.where(users_id: current_user.id)
   end
   def show
     @survey = Survey.find(params[:id])
-    print "DEBUG INFO HERE"
-    print @survey
-    @question = @survey.questions_id
+    @question1 = Question.find_by id: @survey.question1
+    @question2 = Question.find_by id: @survey.question2
+    @question3 = Question.find_by id: @survey.question3
   end
   def new
     @survey = Survey.new
   end
   def create
-    if current_user
-      @users = User.all
-      for u in @users.size
-        if u.company_id == current_user.company_id
-          @question1 = Question.new
-          @question1.text = params[:question1]
-          @question1.save
+      @question1 = Question.new
+      @question1.text = params[:question1]
+      @question1.save
 
-          @question2 = Question.new
-          @question2.text = params[:question2]
-          @question2.save
+      @question2 = Question.new
+      @question2.text = params[:question2]
+      @question2.save
 
-          @question3 = Question.new
-          @question3.text = params[:question3]
-          @question3.save
+      @question3 = Question.new
+      @question3.text = params[:question3]
+      @question3.save
 
-          @survey = Survey.new
-          @survey.question1 = @question1.id
-          @survey.question2 = @question2.id
-          @survey.question3 = @question3.id
+      @users = User.where(company_id: current_user.company_id)
+      @users.each do |u|
+        @survey = Survey.new
+        @survey.taken = false
+        @survey.users_id = u.id
+        @survey.question1 = @question1.id
+        @survey.question2 = @question2.id
+        @survey.question3 = @question3.id
 
-          @survey.save
-#          if @survey.save
-#            redirect_to surveys_path
-#          else
-#            redirect_to 'create_surveys'
-#          end
-        end
+        @survey.save
       end
-      redirect_to 'create_surveys'
-    end
+      redirect_to "/"
   end
 end
